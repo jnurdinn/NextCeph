12c4e5fbb7f6d1191447b2c69ca4743257d7505f
Jungman Berliansyah <36107365+mamanberliansyah@users.noreply.github.com>
2019-02-26 17:53:43 +0700

    added monitoring for OSDs, MONs, & Pools

--- nextceph/templates/mon.php
+++ nextceph/templates/mon.php
@@ -16,7 +16,6 @@ style('nextceph', 'style');
 			$url = 'https://'.$nc_config['mgr_host'].':'.$nc_config['mgr_port'].'/mon';
 			$login = $nc_config['user'];
 			$pass = $nc_config['psswd'];
-
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL,$url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -26,8 +25,31 @@ style('nextceph', 'style');
 			curl_setopt($ch, CURLOPT_USERPWD, "$login:$pass");
 			$result = curl_exec($ch);
 			curl_close($ch);
-			echo('<pre><H1>MONs</H1>');
-			echo($result);
+			$data = json_decode($result);
+
+			echo "<pre><H1>Monitor Daemons</H1>\n";
+	    echo "<table>";
+			echo "<tr>";
+			echo "<td>Hostname</td>";
+			echo "<td>Rank</td>";
+			echo "<td>Quorum</td>";
+			echo "<td>Leader</td>";
+			echo "<td>Host Address</td>";
+			echo "</tr>";
+      // Cycle through the array
+      foreach ($data as $idx => $stand) {
+	      // Output a row
+	      echo "<tr>";
+	      echo "<td>$stand->server</td>";
+				echo "<td>$stand->rank</td>";
+				echo "<td>$stand->in_quorum</td>";
+				echo "<td>$stand->leader</td>";
+				echo "<td>$stand->public_addr</td>";
+	      echo "</tr>";
+
+      // Close the table
+	    }
+			echo "</table>";
 			echo('</pre>');
 			?>
 		</div>
--- nextceph/templates/osd.php
+++ nextceph/templates/osd.php
@@ -3,14 +3,14 @@ script('nextceph', 'script');
 style('nextceph', 'style');
 ?>
 
-<div id="app">
-	<div id="app-navigation">
+<div id='app'>
+	<div id='app-navigation'>
 		<?php print_unescaped($this->inc('navigation/index')); ?>
 		<?php print_unescaped($this->inc('settings/index')); ?>
 	</div>
 
-	<div id="app-content">
-		<div id="app-content-wrapper">
+	<div id='app-content'>
+		<div id='app-content-wrapper'>
 			<?php
 			include 'config/config.php';
 			$url = 'https://'.$nc_config['mgr_host'].':'.$nc_config['mgr_port'].'/osd';
@@ -25,8 +25,28 @@ style('nextceph', 'style');
 			curl_setopt($ch, CURLOPT_USERPWD, "$login:$pass");
 			$result = curl_exec($ch);
 			curl_close($ch);
-			echo('<pre><H1>OSDs</H1>');
-			echo($result);
+			$data = json_decode($result);
+			echo "<pre><H1>Object Storage Daemons</H1>\n";
+			echo "<table>";
+			echo "<tr>";
+			echo "<td>OSD ID</td>";
+			echo "<td>Hostname</td>";
+			echo "<td>Cluster Address</td>";
+			echo "<td>Public Address</td>";
+			echo "</tr>";
+      // Cycle through the array
+      foreach ($data as $idx => $stand) {
+	      // Output a row
+	      echo "<tr>";
+	      echo "<td>$stand->osd</td>";
+				echo "<td>$stand->server</td>";
+				echo "<td>$stand->cluster_addr</td>";
+				echo "<td>$stand->public_addr</td>";
+	      echo "</tr>";
+
+			// Close the table
+			}
+			echo '</table>';
 			echo('</pre>');
 			?>
 		</div>
